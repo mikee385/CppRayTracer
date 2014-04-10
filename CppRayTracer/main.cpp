bd994fb57e57f1bbb4997b455e281dd5d828bca6
mikee385 <mikee385@gmail.com>
2014-04-10 10:47:22 -0500

    Uses the stack for allocation of the scene objects, instead of the heap (except the grid spheres, which are allocated using a vector).

--- CppRayTracer/main.cpp
+++ CppRayTracer/main.cpp
@@ -5,7 +5,6 @@
 
 #include <chrono>
 #include <iostream>
-#include <memory>
 #include <vector>
 
 using namespace std;
@@ -28,66 +27,66 @@ int main()
 
     Scene scene(ColorRGB(2.0f, 2.0f, 2.0f), 1.0f, 5);
 
-    std::unique_ptr<Sphere> groundSphere(new Sphere(Point3D(0, -10004, 20), 10000, MaterialBuilder()
+    Sphere groundSphere(Point3D(0, -10004, 20), 10000, MaterialBuilder()
         .Color(ColorRGB(0.20f, 0.20f, 0.20f))
         .Diffuse(1.0f)
         .Specular(0.0f)
         .Shininess(0)
         .Reflection(0.0f)
         .Refraction(0.0f)
-        .RefractiveIndex(0.0f)));
-    scene.AddObject(groundSphere.get());
+        .RefractiveIndex(0.0f));
+    scene.AddObject(&groundSphere);
 
-    std::unique_ptr<Sphere> sphere1(new Sphere(Point3D(0, 0, 20), 4, MaterialBuilder()
+    Sphere sphere1(Point3D(0, 0, 20), 4, MaterialBuilder()
         .Color(ColorRGB(1.00f, 0.32f, 0.36f))
         .Diffuse(1.0f)
         .Specular(0.0f)
         .Shininess(0)
         .Reflection(1.0f)
         .Refraction(0.5f)
-        .RefractiveIndex(1.1f)));
-    scene.AddObject(sphere1.get());
+        .RefractiveIndex(1.1f));
+    scene.AddObject(&sphere1);
 
-    std::unique_ptr<Sphere> sphere2(new Sphere(Point3D(5, -1, 15), 2, MaterialBuilder()
+    Sphere sphere2(Point3D(5, -1, 15), 2, MaterialBuilder()
         .Color(ColorRGB(0.90f, 0.76f, 0.46f))
         .Diffuse(1.0f)
         .Specular(0.0f)
         .Shininess(0)
         .Reflection(1.0f)
         .Refraction(0.0f)
-        .RefractiveIndex(0.0f)));
-    scene.AddObject(sphere2.get());
+        .RefractiveIndex(0.0f));
+    scene.AddObject(&sphere2);
 
-    std::unique_ptr<Sphere> sphere3(new Sphere(Point3D(5, 0, 25), 3, MaterialBuilder()
+    Sphere sphere3(Point3D(5, 0, 25), 3, MaterialBuilder()
         .Color(ColorRGB(0.65f, 0.77f, 0.97f))
         .Diffuse(1.0f)
         .Specular(0.0f)
         .Shininess(0)
         .Reflection(1.0f)
         .Refraction(0.0f)
-        .RefractiveIndex(0.0f)));
-    scene.AddObject(sphere3.get());
+        .RefractiveIndex(0.0f));
+    scene.AddObject(&sphere3);
 
-    std::unique_ptr<Sphere> sphere4(new Sphere(Point3D(-5.5, 0, 15), 3, MaterialBuilder()
+    Sphere sphere4(Point3D(-5.5, 0, 15), 3, MaterialBuilder()
         .Color(ColorRGB(0.90f, 0.90f, 0.90f))
         .Diffuse(1.0f)
         .Specular(0.0f)
         .Shininess(0)
         .Reflection(1.0f)
         .Refraction(0.0f)
-        .RefractiveIndex(0.0f)));
-    scene.AddObject(sphere4.get());
+        .RefractiveIndex(0.0f));
+    scene.AddObject(&sphere4);
 
-    std::unique_ptr<SceneLight> lightSource(new SceneLight(Point3D(0, 20, 30), 3.0f, ColorRGB(3.0f, 3.0f, 3.0f)));
-    scene.AddLightSource(lightSource.get());
+    SceneLight lightSource(Point3D(0, 20, 30), 3.0f, ColorRGB(3.0f, 3.0f, 3.0f));
+    scene.AddLightSource(&lightSource);
 
-    std::unique_ptr<Camera> camera(new Camera(width, height, fieldOfView, 1, Point3D::Origin, Point3D(0, 0, 1)));
-    scene.AddCamera(camera.get());
+    Camera camera(width, height, fieldOfView, 1, Point3D::Origin, Point3D(0, 0, 1));
+    scene.AddCamera(&camera);
 
     scene.Render();
 
     PPM_Image image("mine.ppm");
-    image.Save(camera->GetImage());
+    image.Save(camera.GetImage());
 
 #endif
 
@@ -102,43 +101,43 @@ int main()
 
     Scene scene(ColorRGB::Color_Black, 1.0f, 5);
 
-    std::unique_ptr<Plane> groundPlane(new Plane(4.4f, Direction3D(0, 1, 0), MaterialBuilder()
+    Plane groundPlane(4.4f, Direction3D(0, 1, 0), MaterialBuilder()
         .Color(ColorRGB(0.4f, 0.3f, 0.3f))
         .Diffuse(1.0f)
         .Specular(0.0f)
         .Shininess(0)
-        .Reflection(0.0f)));
-    scene.AddObject(groundPlane.get());
+        .Reflection(0.0f));
+    scene.AddObject(&groundPlane);
 
-    std::unique_ptr<Sphere> bigSphere(new Sphere(Point3D(1, -0.8f, 3), 2.5f, MaterialBuilder()
+    Sphere bigSphere(Point3D(1, -0.8f, 3), 2.5f, MaterialBuilder()
         .Color(ColorRGB(0.7f, 0.7f, 0.7f))
         .Diffuse(0.2f)
         .Specular(0.8f)
         .Shininess(20)
-        .Reflection(0.6f)));
-    scene.AddObject(bigSphere.get());
+        .Reflection(0.6f));
+    scene.AddObject(&bigSphere);
 
-    std::unique_ptr<Sphere> smallSphere(new Sphere(Point3D(-5.5f, -0.5, 7), 2, MaterialBuilder()
+    Sphere smallSphere(Point3D(-5.5f, -0.5, 7), 2, MaterialBuilder()
         .Color(ColorRGB(0.7f, 0.7f, 1.0f))
         .Diffuse(0.1f)
         .Specular(0.9f)
         .Shininess(20)
-        .Reflection(1.0f)));
-    scene.AddObject(smallSphere.get());
+        .Reflection(1.0f));
+    scene.AddObject(&smallSphere);
 
-    std::unique_ptr<SceneLight> lightSource1(new SceneLight(Point3D(0, 5, 5), 0.1f, ColorRGB(0.6f, 0.6f, 0.6f)));
-    scene.AddLightSource(lightSource1.get());
+    SceneLight lightSource1(Point3D(0, 5, 5), 0.1f, ColorRGB(0.6f, 0.6f, 0.6f));
+    scene.AddLightSource(&lightSource1);
 
-    std::unique_ptr<SceneLight> lightSource2(new SceneLight(Point3D(2, 5, 1), 0.1f, ColorRGB(0.7f, 0.7f, 0.9f)));
-    scene.AddLightSource(lightSource2.get());
+    SceneLight lightSource2(Point3D(2, 5, 1), 0.1f, ColorRGB(0.7f, 0.7f, 0.9f));
+    scene.AddLightSource(&lightSource2);
 
-    std::unique_ptr<Camera> camera(new Camera(width, height, 8, 6, 5, Point3D(0, 0, -5), Point3D(0, 0, 1)));
-    scene.AddCamera(camera.get());
+    Camera camera(width, height, 8, 6, 5, Point3D(0, 0, -5), Point3D(0, 0, 1));
+    scene.AddCamera(&camera);
 
     scene.Render();
 
     PPM_Image image("mine.ppm");
-    image.Save(camera->GetImage());
+    image.Save(camera.GetImage());
 
 #endif
 
@@ -153,98 +152,99 @@ int main()
 
     Scene scene(ColorRGB::Color_Black, 1.0f, 5);
 
-    std::unique_ptr<Plane> groundPlane(new Plane(4.4f, Direction3D(0, 1, 0), MaterialBuilder()
+    Plane groundPlane(4.4f, Direction3D(0, 1, 0), MaterialBuilder()
         .Color(ColorRGB(0.4f, 0.3f, 0.3f))
         .Diffuse(1.0f)
         .Specular(0.8f)
         .Shininess(20)
         .Reflection(0.0f)
         .Refraction(0.0f)
-        .RefractiveIndex(0.0f)));
-    scene.AddObject(groundPlane.get());
+        .RefractiveIndex(0.0f));
+    scene.AddObject(&groundPlane);
 
-    std::unique_ptr<Sphere> bigSphere(new Sphere(Point3D(2, 0.8f, 3), 2.5f, MaterialBuilder()
+    Sphere bigSphere(Point3D(2, 0.8f, 3), 2.5f, MaterialBuilder()
         .Color(ColorRGB(0.7f, 0.7f, 1.0f))
         .Diffuse(0.2f)
         .Specular(0.8f)
         .Shininess(20)
         .Reflection(0.2f)
         .Refraction(0.8f)
-        .RefractiveIndex(1.3f)));
-    scene.AddObject(bigSphere.get());
+        .RefractiveIndex(1.3f));
+    scene.AddObject(&bigSphere);
 
-    std::unique_ptr<Sphere> smallSphere(new Sphere(Point3D(-5.5f, -0.5, 7), 2, MaterialBuilder()
+    Sphere smallSphere(Point3D(-5.5f, -0.5, 7), 2, MaterialBuilder()
         .Color(ColorRGB(0.7f, 0.7f, 1.0f))
         .Diffuse(0.1f)
         .Specular(0.8f)
         .Shininess(20)
         .Reflection(0.5f)
         .Refraction(0.0f)
-        .RefractiveIndex(1.3f)));
-    scene.AddObject(smallSphere.get());
+        .RefractiveIndex(1.3f));
+    scene.AddObject(&smallSphere);
 
-    std::unique_ptr<SceneLight> lightSource1(new SceneLight(Point3D(0, 5, 5), 0.1f, ColorRGB(0.4f, 0.4f, 0.4f)));
-    scene.AddLightSource(lightSource1.get());
+    SceneLight lightSource1(Point3D(0, 5, 5), 0.1f, ColorRGB(0.4f, 0.4f, 0.4f));
+    scene.AddLightSource(&lightSource1);
 
-    std::unique_ptr<SceneLight> lightSource2(new SceneLight(Point3D(-3, 5, 1), 0.1f, ColorRGB(0.6f, 0.6f, 0.8f)));
-    scene.AddLightSource(lightSource2.get());
+    SceneLight lightSource2(Point3D(-3, 5, 1), 0.1f, ColorRGB(0.6f, 0.6f, 0.8f));
+    scene.AddLightSource(&lightSource2);
 
-    std::unique_ptr<Sphere> extraSphere(new Sphere(Point3D(-1.5f, -3.8f, 1), 1.5f, MaterialBuilder()
+    Sphere extraSphere(Point3D(-1.5f, -3.8f, 1), 1.5f, MaterialBuilder()
         .Color(ColorRGB(1.0f, 0.4f, 0.4f))
         .Diffuse(0.2f)
         .Specular(0.8f)
         .Shininess(20)
         .Reflection(0.0f)
         .Refraction(0.8f)
-        .RefractiveIndex(1.5f)));
-    scene.AddObject(extraSphere.get());
+        .RefractiveIndex(1.5f));
+    scene.AddObject(&extraSphere);
 
-    std::unique_ptr<Plane> backPlane(new Plane(12, Vector3D(0.4f, 0, -1), MaterialBuilder()
+    Plane backPlane(12, Vector3D(0.4f, 0, -1), MaterialBuilder()
         .Color(ColorRGB(0.5f, 0.3f, 0.5f))
         .Diffuse(0.6f)
         .Specular(0.0f)
         .Shininess(0)
         .Reflection(0.0f)
         .Refraction(0.0f)
-        .RefractiveIndex(0.0f)));
-    scene.AddObject(backPlane.get());
+        .RefractiveIndex(0.0f));
+    scene.AddObject(&backPlane);
 
-    std::unique_ptr<Plane> ceilingPlane(new Plane(7.4f, Direction3D(0, -1, 0), MaterialBuilder()
+    Plane ceilingPlane(7.4f, Direction3D(0, -1, 0), MaterialBuilder()
         .Color(ColorRGB(0.4f, 0.7f, 0.7f))
         .Diffuse(0.5f)
         .Specular(0.0f)
         .Shininess(0)
         .Reflection(0.0f)
         .Refraction(0.0f)
-        .RefractiveIndex(0.0f)));
-    scene.AddObject(ceilingPlane.get());
+        .RefractiveIndex(0.0f));
+    scene.AddObject(&ceilingPlane);
 
-    std::vector<std::unique_ptr<Sphere>> gridSpheres;
+    std::vector<Sphere> gridSpheres;
     for (size_t x = 0; x < 8; x++)
     {
         for (size_t y = 0; y < 7; y++)
         {
-            std::unique_ptr<Sphere> gridSphere(new Sphere(Point3D(-4.5f + x * 1.5f, -4.3f + y * 1.5f, 10), 0.3f, MaterialBuilder()
+            gridSpheres.emplace_back(Point3D(-4.5f + x * 1.5f, -4.3f + y * 1.5f, 10), 0.3f, MaterialBuilder()
                 .Color(ColorRGB(0.3f, 1.0f, 0.4f))
                 .Diffuse(0.6f)
                 .Specular(0.6f)
                 .Shininess(20)
                 .Reflection(0.0f)
                 .Refraction(0.0f)
-                .RefractiveIndex(0.0f)));
-            scene.AddObject(gridSphere.get());
-
-            gridSpheres.emplace_back(std::move(gridSphere));
+                .RefractiveIndex(0.0f));
         }
     }
+    for (std::vector<Sphere>::const_iterator gridSphere = gridSpheres.begin(), end = gridSpheres.end(); gridSphere != end; ++gridSphere)
+    {
+        scene.AddObject(&(*(gridSphere)));
+    }
 
-    std::unique_ptr<Camera> camera(new Camera(width, height, 8, 6, 5, Point3D(0, 0, -5), Point3D(0, 0, 1)));
-    scene.AddCamera(camera.get());
+    Camera camera(width, height, 8, 6, 5, Point3D(0, 0, -5), Point3D(0, 0, 1));
+    scene.AddCamera(&camera);
 
     scene.Render();
 
     PPM_Image image("mine.ppm");
-    image.Save(camera->GetImage());
+    image.Save(camera.GetImage());
 
 #endif
 
